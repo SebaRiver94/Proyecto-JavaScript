e964ba8457f139d39161a3cf90797aee6405ee51
Sebastian-Java <sebastiandonofrio94@gmail.com>
2025-09-05 20:39:17 -0300

    primer commit java

---
+++ main.js
@@ -0,0 +1,11 @@
+let nombre = "Sebastian";
+console.log(nombre);
+
+let edad = 31;
+console.log(edad);
+
+const fechaNacimiento = "28/08/1994";
+console.log(fechaNacimiento);
+
+let ciudad = "Buenos Aires - Argentina";
+console.log(ciudad);
